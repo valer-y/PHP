cf83fc41c5c105254cd37565251671c118603175
Yazykov Valery <valery.yazykov@catenamedia.com>
2023-07-17 18:34:34 +0200

    feature_1 pull out data from DB

--- app/public/index.php
+++ app/public/index.php
@@ -1,19 +1,6 @@
 <?php
 
+require_once "../vendor/autoload.php";
 
-$dsn = "mysql:host=mysql;dbname=data";
-$db_username = "root";
-$db_password = "12345678";
-
-$sql = "CREATE TABLE Goods (
-    id INT(6) AUTO_INCREMENT UNSIGNED PRIMARY KEY
-  )";
-
-try {
-	$pdo = new PDO($dsn, $db_username, $db_password);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo('Connection');
-	echo('Db created');
-} catch (PDOException $e) {
-	echo $e->getMessage();
-}
+$test = new \App\Test();
+$test->getInfo();
