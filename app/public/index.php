1c3aee11b212308dc8dd12f18c4d099cdbedace7
Yazykov Valery <valery.yazykov@catenamedia.com>
2023-07-19 05:39:38 +0200

    feature-2 html layout

--- app/public/index.php
+++ app/public/index.php
@@ -2,15 +2,74 @@
 
 require_once "../vendor/autoload.php"; ?>
 
+<?php
+
+$host   = 'mysql';
+$user   = 'root';
+$pwd 	= '12345678';
+$dbName = 'products_crud';
+
+$dsn = "mysql:host=$host;port=3306;dbname=$dbName";
+
+$pdo = new PDO($dsn, $user, $pwd);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query = "SELECT * FROM products ORDER BY create_date DESC";
+
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//echo '<pre>';
+//var_dump($products);
+//echo '</pre>';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Bootstrap demo</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-<h1>Products CRUD demo</h1>
+    <h1>Products CRUD demo</h1>
+
+    <p>
+        <a href="create.php" class="btn btn-success">Create Product</a>
+    </p>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Image</th>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+            <th scope="col">Create</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+
+            <?php foreach ($products as $i => $product) : ?>
+                <tr>
+                    <th scope="row"><?php echo ++$i; ?></th>
+                    <td></td>
+                    <td><?php echo $product['title']; ?></td>
+                    <td><?php echo $product['price']; ?></td>
+                    <td><?php echo $product['create_date']; ?></td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
+                        <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+
+                </tr>
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
 </body>
 </html>
