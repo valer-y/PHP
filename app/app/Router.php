3aaf17bcb2a19fd364afee66ada3a07fd191070d
Yazykov Valery <valery.yazykov@catenamedia.com>
2023-07-24 05:53:48 +0200

    feature-8 create router

---
+++ app/app/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+class Router
+{
+	public array $getRoutes = [];
+	public array $postRoutes = [];
+
+	public function get($url, $fn)
+	{
+		$this->getRoutes[$url] = $fn;
+	}
+
+	public function post($url, $fn)
+	{
+		$this->postRoutes[$url] = $fn;
+	}
+
+	public function resolve()
+	{
+		$currentUrl = $_SERVER['REQUEST_URI'] ?? '/';
+		$method = $_SERVER['REQUEST_METHOD'];
+
+		if ($method === 'GET') {
+			$fn = $this->getRoutes[$currentUrl] ?? null;
+		} else {
+			$fn = $this->postRoutes[$currentUrl] ?? null;
+		}
+
+		if ($fn) {
+			echo "<pre>";
+			var_dump($fn);
+			echo "</pre>";
+			echo "<br><br>";
+			var_dump($_SERVER);
+		} else {
+			echo "Page not found";
+		}
+	}
+}
