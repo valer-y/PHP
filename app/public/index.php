d65bf78fe7015ca83f9c29121f6dbfc9ea69c316
Yazykov Valery <valery.yazykov@catenamedia.com>
2023-07-27 17:02:12 +0200

    feature-1 create routes

--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,18 @@
 <?php
+
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+use App\Router;
+use App\Controllers\ProductController;
+
+$router = new Router();
+
+$router->get('/', [ProductController::class, 'index'])
+	->get('/products', [ProductController::class, 'create'])
+	->get('/products/create', [ProductController::class, 'create'])
+	->post('/products/create', [ProductController::class, 'create'])
+	->get('/products/update', [ProductController::class, 'update'])
+	->post('/products/update', [ProductController::class, 'update'])
+	->post('/products/update', [ProductController::class, 'updelete']);
+
+$router->resolve();
