2e119c991c2d592e231f020238af9fb9a17ba7cd
Valery Yazykov <valery.yazykov@interexy.com>
2023-08-26 17:27:24 +0300

    _SERVER

---
+++ app/app/Classes/Invoice.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Classes;
+
+class Invoice
+{
+    public function index() : string
+    {
+        return 'Invoicse';
+    }
+
+    public function create() : string
+    {
+        return 'Invoicse Create';
+    }
+}
