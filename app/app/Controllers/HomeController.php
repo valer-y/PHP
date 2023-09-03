2121cdc83015fa323157cfcd6759d02dda4ad112
Valery Yazykov <valery.yazykov@interexy.com>
2023-09-03 13:09:25 +0300

    headers

--- app/app/Controllers/HomeController.php
+++ app/app/Controllers/HomeController.php
@@ -11,10 +11,21 @@ class HomeController
         return View::make('index', ['header' => 'bar']);
     }
 
+    public function download()
+    {
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment;filename="myfile.pdf"');
+
+        readfile(STORAGE_PATH . '/receipt 6-20-2021.pdf');
+    }
+
+
+
     public function upload()
     {
-//        var_dump($_FILES);
         $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
         move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
+
+        header('Location: /');
     }
 }
--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 session_start();
@@ -9,14 +10,24 @@ define('VIEW_PATH', dirname(__DIR__) . '/views/');
 
 $router = new \App\Router();
 
-$router
-    ->get('/', [\App\Controllers\HomeController::class, 'index'])
-    ->post('/upload', [\App\Controllers\HomeController::class, 'upload'])
-    ->get('/invoices', [\App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoices/create', [\App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoices/create', [\App\Controllers\InvoiceController::class, 'store']);
+try {
 
-echo $router->resolve(
-    $_SERVER['REQUEST_URI'],
-    strtolower($_SERVER['REQUEST_METHOD'])
-);
+    $router
+        ->get('/', [\App\Controllers\HomeController::class, 'index'])
+        ->get('/download', [\App\Controllers\HomeController::class, 'download'])
+        ->post('/upload', [\App\Controllers\HomeController::class, 'upload'])
+        ->get('/invoices', [\App\Controllers\InvoiceController::class, 'index'])
+        ->get('/invoices/create', [\App\Controllers\InvoiceController::class, 'create'])
+        ->post('/invoices/create', [\App\Controllers\InvoiceController::class, 'store']);
+
+    echo $router->resolve(
+        $_SERVER['REQUEST_URI'],
+        strtolower($_SERVER['REQUEST_METHOD'])
+    );
+
+} catch (\App\Exception\RouteNotFoundException $e) {
+    header('HTTP/1.1 404 Not Found');
+    http_response_code(404);
+
+    echo \App\View::make('error/404');
+}
