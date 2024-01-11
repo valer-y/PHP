9194d85c65416f18d83ffc8b0143cf4737985387
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-11 04:34:29 +0100

    feature-36: create default class, check stdClass

---
+++ src/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+
+class Transaction
+{
+    private float $amount;
+    private string $description;
+
+    public function __construct(float $amount, string $description)
+    {
+        $this->amount = $amount;
+        $this->description = $description;
+    }
+
+    public function addTax(float $rate) : Transaction
+    {
+        $this->amount += $this->amount * $rate / 100;
+
+        return $this;
+    }
+
+    public function applyDiscount(float $rate) : Transaction
+    {
+        $this->amount -= $this->amount * $rate / 100;
+
+        return $this;
+    }
+
+    public function getAmount() {
+        return $this->amount;
+    }
+
+    public function __destruct()
+    {
+        echo 'Destruct ' . $this->description . '<br />';
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,21 @@
 <?php
 
-phpinfo();
+declare(strict_types=1);
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+require_once "../Transaction.php";
+
+$transaction1 = (new Transaction(100, 'Transaction 1'))
+    ->addTax(8)
+    ->applyDiscount(10);
+
+var_dump($transaction1->getAmount());
+
+unset($transaction1);
+
+echo '<br /><br />';
+
+$arr = [1, 2, 3];
+
+$obj = (object) $arr;
+
+var_dump($obj->{1});
