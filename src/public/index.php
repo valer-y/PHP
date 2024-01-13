bdbc59e35bf85107a2233848a0f359e558468e82
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-13 11:40:50 +0100

    feature-41: static props and methods, singleton intro

--- src/public/index.php
+++ src/public/index.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\Transaction;
-use App\Enums\Status;
 
-$transaction = new Transaction();
+$transaction = new Transaction(25, 'Transaction1');
+$transaction = new Transaction(25, 'Transaction1');
+$transaction = new Transaction(25, 'Transaction1');
+$transaction = new Transaction(25, 'Transaction1');
 
-$transaction->setStatus(Status::DECLINED);
 
-var_dump($transaction);
+var_dump(Transaction::getCounter());
