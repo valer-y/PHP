f53109beccc5efd409349b7af5a531b587cd2979
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-14 06:09:57 +0100

    feature-43: Inheritancs, composition intro, is-a

---
+++ src/app/ToasterPro.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class ToasterPro extends Toaster
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->size = 4;
+    }
+
+    public function toastBagel()
+    {
+        foreach ($this->slices as $i => $slice)
+        {
+            echo ($i + 1) . ' : Toasting ' . $slice .  'with bage option' . PHP_EOL;
+        }
+    }
+}
