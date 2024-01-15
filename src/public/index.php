e94edbaa6640ee36c35b2a693e614e1020b13906
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-15 07:18:15 +0100

    feature-45: interfaces, polymorphism

--- src/public/index.php
+++ src/public/index.php
@@ -4,27 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\ToasterPro;
-use App\FancyOven;
-use App\PaymentGateway\Paddle\Transaction;
+use App\CollectionAgency;
 
-$toaster = new ToasterPro();
+$collector = new \App\DebtCollectionService();
 
-
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-
-$toaster->toast();
-
-$fields = [
-    //new \App\Field('baseField'), // Abstract ?
-    new \App\Text('textField'),
-    new \App\Radio('radioField')
-];
-
-foreach ($fields as $field) {
-    echo $field->render() . '<br />';
-}
+echo $collector->collectDebt(new CollectionAgency) . PHP_EOL;
