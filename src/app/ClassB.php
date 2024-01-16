760e7e4e152ae7326d2350411efacfcb34c22b8c
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-16 04:10:58 +0100

    feature-47: self, static

---
+++ src/app/ClassB.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class ClassB extends ClassA
+{
+    protected static string $name = 'B';
+}
--- src/public/index.php
+++ src/public/index.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Invoice;
+$classA = new \App\ClassA();
+$classB = new \App\ClassB();
 
-$invoice = new Invoice(15);
-
-echo $invoice->amount . PHP_EOL;
-
-//$invoice->status = 15;
+echo $classA->getName() . PHP_EOL;
+echo $classB->getName() . PHP_EOL;
