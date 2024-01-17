78ba810af1407912ef33cbc0d2eec6c9ea38c2ee
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-17 07:10:56 +0100

    feature-50: variablew storage and object comapison

--- src/public/index.php
+++ src/public/index.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$coffeeMaker = new \App\CoffeeMaker();
-$coffeeMaker->makeCoffe();
-
-$latteMaker = new \App\LatteMaker();
-$latteMaker->makeCoffe();
-$latteMaker->makeLatte();
-
-$allInOneCoffeeMaker = new \App\AllInOneCoffeMaker();
-$allInOneCoffeeMaker->makeLatte();
-$allInOneCoffeeMaker->makeCappuccino();
+//$coffeeMaker = new \App\CoffeeMaker();
+//$coffeeMaker->makeCoffe();
+//
+//$latteMaker = new \App\LatteMaker();
+//$latteMaker->makeCoffe();
+//$latteMaker->makeLatte();
+//
+//$allInOneCoffeeMaker = new \App\AllInOneCoffeMaker();
+//$allInOneCoffeeMaker->makeLatte();
+//$allInOneCoffeeMaker->makeCappuccino();
