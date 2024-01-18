e6209e0fc31280286542e8d0400da453fdbbc7cd
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-18 05:04:00 +0100

    feature-53: serialize/unserialize

--- src/public/index.php
+++ src/public/index.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$invoice = new \App\Invoice();
+$invoice = new \App\Invoice(25, 'Invoice1', '1290424980249');
 
-$invoice2 = new $invoice;
+$str = serialize($invoice);
 
-var_dump($invoice, $invoice2, \App\Invoice::create());
+$invoice2 = unserialize($str);
+
+var_dump($invoice2);
