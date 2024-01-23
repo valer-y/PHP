355b235565978b0cb59aeddeadc30dca84609406
Valery Yazykov <valery.yazykov@interexy.com>
2024-01-23 04:33:40 +0100

    feature-56: iterator

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -8,28 +8,10 @@ use http\Exception\RuntimeException;
 
 class Invoice
 {
-    public function __construct(
-        public Customer $customer
-    )
-    {
-    }
+   public string $id;
 
-    public function process(float $amount) : void
-    {
-        if($amount <= 0) {
-            throw new \Exception('Invalid amount');
-        }
-
-        if(empty($this->customer->getBillingInfo())) {
-            throw new MissingBillingException();
-        }
-
-        echo 'Processing $' . $amount . ' invoice - ';
-
-        sleep(1);
-
-        echo 'OK' . PHP_EOL;
-    }
-
-
+   public function __construct(public float $amount)
+   {
+       $this->id = random_int(100, 99999);
+   }
 }
