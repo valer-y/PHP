264c7891954a348949514bb2076553e22358bff1
Valery Yazykov <valery.yazykov@interexy.com>
2024-02-01 04:23:43 +0100

    feature-2: add PDO support

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+//$pdo = new PDO('mysql:host=sandbox-db;port=3306;dbname=my_db', 'root', 'root');
+//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//
+//$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+//$statement->execute();
+//$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+//
+//var_dump($products);
+
+//phpinfo();
 ?>
 
 <!doctype html>
@@ -12,6 +22,7 @@ require __DIR__ . '/../vendor/autoload.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Train CRUD</title>
+    <link rel="stylesheet" href="app.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
