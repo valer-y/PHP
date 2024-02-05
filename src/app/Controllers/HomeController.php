72099b9364b8605b18407871b728930eec102852
Valery Yazykov <valery.yazykov@interexy.com>
2024-02-05 04:17:52 +0100

    feature-63: after test initial commit

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -4,12 +4,39 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 use App\View;
+use PDO;
 
 class HomeController
 {
     public function index() : View
     {
-        return View::make('index', ['foo' => 'bar']);
+        try {
+            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+
+            $email = "Nick@doe.com";
+            $name = "Nick Doe";
+            $isActive = 1;
+
+            $stmt = $db->prepare('INSERT INTO users (email, full_name, is_active, created_at) VALUES (?, ?, ?, NOW())');
+            $stmt->execute([$email, $name, $isActive]);
+
+            $id = $db->lastInsertId();
+
+            var_dump($id);
+
+            $user = $db->query("SELECT * FROM users WHERE id = " .$id)->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (\PDOException $e){
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+
+        echo "<pre>";
+        var_dump($user);
+        echo "</pre>";
+
+        return View::make('index');
     }
 
     public function upload()
