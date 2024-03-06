0023f5a1bdeaf966a841f8b4538c98b1f3197832
Valery Yazykov <valery.yazykov@interexy.com>
2024-03-06 05:10:37 +0100

    workflow commit

---
+++ app/Entity/Invoice.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enums\InvoiceStatus;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity]
+#[Table('invoices')]
+class Invoice
+{
+    #[Id]
+    #[Column, GeneratedValue]
+    private int $id;
+
+    #[Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private float $amount;
+
+    #[Column(name: 'invoice_number')]
+    private string $invoiceNumber;
+
+    #[Column]
+    private InvoiceStatus $status;
+
+    #[Column(name: 'created_at')]
+    private \DateTime $createdAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    public function getInvoiceNumber(): string
+    {
+        return $this->invoiceNumber;
+    }
+
+    public function setInvoiceNumber(string $invoiceNumber): void
+    {
+        $this->invoiceNumber = $invoiceNumber;
+    }
+
+    public function getStatus(): InvoiceStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(InvoiceStatus $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
+}
