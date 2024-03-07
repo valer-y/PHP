bed633d8a2c9d458776ffbb60a7bd4f373e89ee4
Valery Yazykov <valery.yazykov@interexy.com>
2024-03-07 07:11:05 +0100

    feature-89: curl

--- app/Controllers/CurlController.php
+++ app/Controllers/CurlController.php
@@ -11,6 +11,30 @@ class CurlController
     #[Get('/curl')]
     public function index()
     {
+        $handle = curl_init();
+
+        $apiKey = $_ENV['EMAILABLE_API_KEY'];
+        $email = "yazykov.valery@gmail.com";
+
+        $params = [
+            'api_key' => $apiKey,
+            'email' => $email
+        ];
+
+        $url = 'https://api.emailable.com/v1/verify?' . http_build_query($params);
+
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+        $content = curl_exec($handle);
+
+        if(false !== $content) {
+            $data = json_decode($content);
+
+            echo "<pre>";
+            print_r($data);
+        }
+
 
     }
 }
